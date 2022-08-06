0eb373669815fa6ef05f7d8f34056cdb12d2a907
Emerson Millard <emillard.code@gmail.com>
2022-08-06 12:00:00 -0700

    Modified main class.

--- src/main/java/com/project/AngularSpringJt2Application.java
+++ src/main/java/com/project/AngularSpringJt2Application.java
@@ -1,4 +1,4 @@
-package com.project.angularspringjt2;
+package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AngularSpringJt2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AngularSpringJt2Application.class, args);
+
     }
 
 }
