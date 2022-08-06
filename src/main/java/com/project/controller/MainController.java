d034dff4a6046888797d17b7c15b70b99214d0c5
Emerson Millard <emillard.code@gmail.com>
2022-08-06 12:00:00 -0700

    Modified controller class.

--- src/main/java/com/project/controller/MainController.java
+++ src/main/java/com/project/controller/MainController.java
@@ -1,4 +1,24 @@
 package com.project.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Book;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
 public class MainController {
+
+    @GetMapping("/findAllBooks")
+    public List<Book> getBooks() {
+        return Stream.of(
+                new Book(101, "Java", 999),
+                new Book(102, "Spring", 1199),
+                new Book(103, "Hibernate", 445),
+                new Book(104, "Angular", 888)
+                        .collect(Collectors.toList()));
+    }
+
 }
