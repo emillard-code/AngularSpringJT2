3430ba1214321a4749481ced3ca2cc7eee04003a
Emerson Millard <emillard.code@gmail.com>
2022-08-07 12:00:00 -0700

    Modified model class.

--- src/main/java/com/project/model/Book.java
+++ src/main/java/com/project/model/Book.java
@@ -1,4 +1,16 @@
 package com.project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
+
+    private int id;
+    private String name;
+    private int price;
+
 }
