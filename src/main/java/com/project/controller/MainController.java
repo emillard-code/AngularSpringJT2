cea1a1a6c2ccb8aae94563fbef16f09922b18b57
Emerson Millard <emillard.code@gmail.com>
2022-08-07 12:00:00 -0700

    Added @CrossOrigin annotation.

--- src/main/java/com/project/controller/MainController.java
+++ src/main/java/com/project/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.project.controller;
 
 import com.project.model.Book;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.util.stream.Stream;
 @RestController
 public class MainController {
 
+    @CrossOrigin("http://localhost:4200/")
     @GetMapping("/findAllBooks")
     public List<Book> getBooks() {
         return Stream.of(
