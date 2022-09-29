6cbac75ed272b182d5a68ece6ebacc23f5d93eb7
Emerson Millard <emillard.code@gmail.com>
2022-09-29 15:56:32 -0700

    Initial commit

---
+++ src/main/java/com/project/angularspringjt2/AngularSpringJt2Application.java
@@ -0,0 +1,13 @@
+package com.project.angularspringjt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringJt2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularSpringJt2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/angularspringjt2/AngularSpringJt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.angularspringjt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringJt2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
